b2ac7c00fb2f61b25a4b8af278296686990b838c
Quoc Bao Dinh Le <qle17@student.gsu.edu>
2024-01-10 11:07:40 -0500

    2nd commit

--- Week 1/Java/binarySearch/src/Main.java
+++ Week 1/Java/binarySearch/src/Main.java
@@ -1,36 +1,24 @@
 
 public class Main {
     public static void main(String[] args) {
-        // The array
-        int[] b = {1, 2, 3, 4, 5};
-        // The number to search
-        int a = 3;
-        // Using binary search
-        int result = binarySearch(b, a);
-        // Print the result
-        System.out.println(result);
+        int[] b = {1, 2, 3, 4, 5}; // The array
+        int a = 3;// The number to search
+        int result = binarySearch(b, a);// Using binary search
+        System.out.println(result);// Print the result
     }
 
     // Binary search function
     public static int binarySearch(int[] b, int a) {
-        // The array low and high
-        int low = 0;
+        int low = 0;// The array low and high
         int high = b.length - 1;
-        // Checks if the array is empty or not
-        while (low <= high) {
-            // The middle of the array
-            int mid = (low + high) / 2;
-            // Checks if the number is in the middle
-            if (a == b[mid]) {
-                return mid;
-                // Checks if the number is less than the middle
-            } else if (a < b[mid]) {
-                // Sets the high to the middle - 1
-                high = mid - 1;
-                // Checks if the number is greater than the middle
-            } else if (a > b[mid]) {
-                // Sets the low to the middle + 1
-                low = mid + 1;
+        while (low <= high) {// Checks if the array is empty or not
+            int mid = (low + high) / 2;// The middle of the array           
+            if (a == b[mid]) { // Checks if the number is in the middle
+                return mid;            
+            } else if (a < b[mid]) { // Checks if the number is less than the middle              
+                high = mid - 1;// Sets the high to the middle - 1             
+            } else if (a > b[mid]) {// Checks if the number is greater than the middle           
+                low = mid + 1; // Sets the low to the middle + 1
             }
         }
         // Returns -1 if the number is not in the array
