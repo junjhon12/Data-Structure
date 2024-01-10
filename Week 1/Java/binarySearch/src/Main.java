c5929fba937eae3b44d348bbd093197ccfe342c7
Quoc Bao Dinh Le <qle17@student.gsu.edu>
2024-01-10 16:24:17 -0500

    fixed in python level two version

--- Week 1/Java/binarySearch/src/Main.java
+++ Week 1/Java/binarySearch/src/Main.java
@@ -1,14 +1,34 @@
 
 public class Main {
     public static void main(String[] args) {
+        /*  
+        // Monday
         int[] b = {1, 2, 3, 4, 5}; // The array
-        int a = 3;// The number to search
+        int a = 1;// The number to search
         int result = binarySearch(b, a);// Using binary search
-        System.out.println(result);// Print the result
-    }
+        if (result == -1) {// Checks if the number is in the array
+            System.out.println("The number is not in the array");// Print the result
+        } else {
+            System.out.println("The number is in the array");// Print the result
+        }*/
 
+        // Tuesday - Binary search where the array is rotated or not sorted
+        int[] c = {4, 5, 6, 7, 9, 0, 1, 2};
+        int a = 5;
+        int result = binarySearch_Lvl2(c, a);
+        if (result == -1) {
+            System.out.println("The number is not in the array");
+        } else {
+            System.out.println("The number is in the array " + result);
+        }
+
+        
+    }   
+
+    
+    //Monday
     // Binary search function
-    public static int binarySearch(int[] b, int a) {
+    public static int binarySearch_Lvl1(int[] b, int a) {
         int low = 0;// The array low and high
         int high = b.length - 1;
         while (low <= high) {// Checks if the array is empty or not
@@ -24,4 +44,33 @@ public class Main {
         // Returns -1 if the number is not in the array
         return -1;
     }
+
+    //Tuesday
+    // Binary search function where the array is rotated or not sorted
+    public static int binarySearch_Lvl2(int[] c, int a) {
+        int low = 0; // The array low and high
+        int high = c.length - 1;
+        while (low <= high) { // Checks if the array is empty or not
+            int mid = low + (high - low) / 2; // The middle of the array
+            if (a == c[mid]) { // Checks if the number is in the middle
+                return mid; 
+            }else if (c[low] <= c[mid]) { // Checks if the array is sorted
+                if (a >= c[mid] || a < c[low]) { // Checks if the number is in the middle, number is greater than the middle, or number is less than the low
+                    low = mid + 1; // Sets the low to the right of the middle
+                }else {  
+                    high = mid - 1; // Sets the high to the left of the middle
+                }
+        }   else {
+                if (a <= c[mid] || a > c[high]) { // Checks if the number is in the middle, number is less than the middle, or number is greater than the high
+                    high = mid - 1; // Sets the high to the left of the middle
+                }else {
+                    low = mid + 1; // Sets the low to the right of the middle
+                }
+            }
+        }  
+        return -1;
+    }
+
+     
+
 }
--- Week 1/Python/binaraySearch.py
+++ Week 1/Python/binaraySearch.py
@@ -1,31 +1,55 @@
  # Bug: overflow due to the mid
 
-def binary_search (a,b):
+# Regular Binary Search
+def binary_search_lvl1(a,b):
     low = 0 #First index
     high = len(b)-1 #Last index
-    
     while low <= high: #Check if valid
-        mid = (low + high) // 2 #Find the middle index
+        mid = low + (high-low)// 2 #Find the middle index
         if a == b[mid]: #If mid is the target
             return f'Target is found at index: {mid}' #Return mid if target is found in the middle
         elif a < b[mid]:#If target is smaller than mid
             high = mid - 1
         else:#If target is greater than mid
             low = mid + 1
-    return f'Target is not found'# If target is not found
+    return 'Target is not found'# If target is not found
 
+# Binaray Search with 
+def binary_search_lvl2(a, b):
+    low = 0
+    high = len(b) - 1
+    while low <= high:
+        mid = low + (high - low) // 2
+        if a == b[mid]:
+            return 'Target is found at index: {}'.format(mid)
+        elif b[low] <= b[mid]:
+            if a >= b[mid] or a < b[low]:
+                low = mid + 1
+            else:
+                high = mid - 1
+        else:
+            if a <= b[mid] or a > b[high]:
+                high = mid - 1
+            else:
+                low = mid + 1
+    return 'Target is not found'
+
+'''
 b = [3, 9, 10, 11, 15, 19]  # sorted array
-a = 16  # target value
-    
-result = binary_search(a,b)
+a = 16  # target value 
+result = binary_search_Lvl1(a,b)
 print(result)
-
 b = []
 a = 15  # target value
-result2 = binary_search(a,b)
+result2 = binary_search_Lvl1(a,b)
 print(result2)
-
 b = [15]
 a = 15  # target value
-result2 = binary_search(a,b)
+result2 = binary_search_Lvl1(a,b)
 print(result2)
+'''
+
+b = [6, 7, 8, 9, 0, 1, 2, 3, 4, 5]
+a = 5
+result = binary_search_lvl2(a,b)
+print(result)
