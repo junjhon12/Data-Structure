5559871f5ebcccbba14a27b7732e844afce0f1e6
Quoc Bao Dinh Le <qle17@student.gsu.edu>
2024-01-14 20:48:42 -0500

    fixed

--- Week 1/Python/binaraySearch.py
+++ Week 1/Python/binaraySearch.py
@@ -1,91 +1,73 @@
- # Bug: overflow due to the mid
+import math
+# Monday : Binary Search Level 1
+def binarySearch_Lvl1(array, target):
+    leftIndex = 0
+    rightIndex = len(array) - 1
+    
+    while leftIndex <= rightIndex:
+        middleIndex = leftIndex + (rightIndex - leftIndex) // 2
+        
+        if target == array[middleIndex]:
+            return f'Found {target} at index {middleIndex}'
+        elif target < array[middleIndex]:
+            rightIndex = middleIndex - 1
+        else:
+            leftIndex = middleIndex + 1
+    return f'{target} not found in the array'
 
-# Regular Binary Search
-def binary_search_lvl1(a,b):
-    low = 0 #First index
-    high = len(b)-1 #Last index
-    while low <= high: #Check if valid
-        mid = low + (high-low)// 2 #Find the middle index
-        if a == b[mid]: #If mid is the target
-            return f'Target is found at index: {mid}' #Return mid if target is found in the middle
-        elif a < b[mid]:#If target is smaller than mid
-            high = mid - 1
-        else:#If target is greater than mid
-            low = mid + 1
-    return 'Target is not found'# If target is not found
-
-# Binaray Search with rotated array
-def binary_search_lvl2(target, array):
-    left = 0
-    right = len(array) - 1
-    while left <= right:
-        mid = left + (right - left) // 2
-        if target == array[mid]:
-            return mid
-        elif array[left] < array[mid]: # left side is sorted
-            if target > array[mid] or target < array[left]: # search right side
-                left = mid + 1 # search right side
+# Tuesday : Binary Search Level 2 rotated array
+def binarySearch_Lvl2(array, target):
+    leftIndex = 0
+    rightIndex = len(array) - 1
+    
+    while leftIndex <= rightIndex:
+        middleIndex = leftIndex + (rightIndex - leftIndex) // 2
+        
+        if target == array[middleIndex]:
+            return f'Found {target} at index {middleIndex}'
+        elif array[leftIndex] <= array[middleIndex]:
+            if target > array[middleIndex] or target < array[leftIndex]:
+                leftIndex = middleIndex + 1
             else:
-                right = mid - 1 # search left side
-        else: # right side is sorted
-            if target < array[mid] or target > array[right]: # search left side
-                right = mid - 1 # search left side
+                rightIndex = middleIndex - 1
+        else:
+            if target < array[middleIndex] or target > array[rightIndex]:
+                rightIndex = middleIndex - 1
             else:
-                left = mid + 1 # search right side
-    return 'Not Found'
+                leftIndex = middleIndex + 1
+    return f'{target} not found in the array'
 
-def find_Minumum (array):
-    left = 0
-    right = len(array)-1
-    while left <= right:
-        mid = left + (right - left) // 2
-        if target == array[mid]:
-            return mid
-        elif array[left] < array[mid]:
-            if target > array[mid] or target < array[left]:
-                left = mid + 1
-                # search from the left side
-                # If 
-                while left <= right:
-                    leftMid = left + (right - left) // 2
-                    if target == array[leftMid]:
-                        return leftMid
-                    elif array[left] < array[leftMid]:
-                        if target > array[leftMid] or target < array[left]:
-                            left = leftMid + 1
-                        else:
-                            right = leftMid - 1
-                    else:
-                        if target < array[leftMid] or target > array[right]:
-                            right = leftMid - 1
-                        else:
-                            left = leftMid + 1
-            else:
-                right = mid - 1
-                
-
-
-
-'''
-b = [3, 9, 10, 11, 15, 19]  # sorted array
-a = 16  # target value 
-result = binary_search_Lvl1(a,b)
-print(result)
-b = []
-a = 15  # target value
-result2 = binary_search_Lvl1(a,b)
-print(result2)
-b = [15]
-a = 15  # target value
-result2 = binary_search_Lvl1(a,b)
-print(result2)
-'''
-
-array = [6, 7, 8, 9, 0, 1, 2, 3, 4, 5]
+# ---------------------------------------------------------------------------------------------
+array = [1,2,3,4,5,6,7,8,9,10]
 target = 2
-result = binary_search_lvl2(target,array)
-print(result)
+print(binarySearch_Lvl1(array, target))
 
-# Wendesday problem
-# Find the minimum value, not the index but the value without using a linear search using binary search
-array = [320, 10000, 3823, 9524, 1]
+# Simulating the binary search
+# array = [1,2,3,4,5]
+# target = 4
+# leftIndex = 0 (1), rightIndex = 4 (5)
+# middleIndex = 0 + (4 - 0) // 2 = 2 (3)
+# if (4 == 3) -> False
+# elif (4 < 3) -> False
+# else (4 > 3) -> True
+# leftIndex = middleIndex + 1 = 3 (4)
+# search between [4, 5]
+# middleIndex = 3 + (4 - 3) // 2 = 3 (4)
+# if (4 == 4) -> True
+
+array = [3,4,5,1,2]
+target = 1
+print(binarySearch_Lvl2(array, target))
+
+# Simulating the binary search for rotated array
+# array = [3,4,5,1,2]
+# target = 1
+# leftIndex = 0 (3), rightIndex = 4 (2) [3,4,5,1,2]
+# middleIndex = 0 + (4 - 0) // 2 = 2 (5) []
+# if (1 == 5) -> False
+# elif (3 <= 5) -> True
+#   if (1 > 5) or (1 < 3) -> True
+#      leftIndex = middleIndex + 1 = 3 (1) [1,2]
+# middelIndex = 3 + (4 - 3) // 2 = 3 (1) 
+# if (1 == 1) -> True
+# return f'Found {target} at index {middleIndex}' -> Found 1 at index 3
