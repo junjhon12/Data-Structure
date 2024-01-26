8f20f6b97ede0450e33a94b2bf338cabd84bcbf7
Quoc Bao Dinh Le <qle17@student.gsu.edu>
2024-01-26 11:57:59 -0500

    lab 3 done

--- Week 3/Java/src/week3.java
+++ Week 3/Java/src/week3.java
@@ -1,8 +1,4 @@
 import java.util.Dictionary;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
 
 public class week3{
     public static void main(String[] args) {
@@ -29,27 +25,13 @@ public class week3{
         // n -> number of elements in input
         // m -> average length
 
-        public class week3 {
-            public static void main(String[] args) {
-                String[] A = {"eat", "tea", "tan", "ate", "nat", "bat"};
-                List<List<String>> result = groupAnagrams(A);
-                System.out.println(result);
+        Dictionary<String, String> dictionary = new Dictionary<String, String>();
+        for (String word : A) {
+            String sortedWord = "".join(sorted(word));
+            if (!dictionary.containsKey(sortedWord)) {
+                dictionary.put(sortedWord, new ArrayList<String>());
             }
-
-            public static List<List<String>> groupAnagrams(String[] A) {
-                HashMap<String, List<String>> dictionary = new HashMap<>();
-                for (String word : A) {
-                    char[] chars = word.toCharArray();
-                    Arrays.sort(chars);
-                    String sortedWord = new String(chars);
-                    if (!dictionary.containsKey(sortedWord)) {
-                        dictionary.put(sortedWord, new ArrayList<>());
-                    }
-                    dictionary.get(sortedWord).add(word);
-                }
-                return new ArrayList<>(dictionary.values());
-            }
+            else
         }
-        return dictionary.values();
     }
 }
