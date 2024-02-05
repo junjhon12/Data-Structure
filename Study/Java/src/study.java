806a72ec9c239d90f2b4e54dac27b4755fa802a0
Quoc Bao Dinh Le <qle17@student.gsu.edu>
2024-02-05 11:09:49 -0500

    update

--- Study/Java/src/study.java
+++ Study/Java/src/study.java
@@ -43,6 +43,36 @@ public class study {
             System.out.println("The target is in the array " + result2);
         }
 
+        /*
+         * Binary Search finding the minimum in both rotated and non-rotated array
+         * 
+         * There is an integer array nums sorted in ascending order (with distinct values).
+         * Prior to being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed).
+         * 
+         * For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+         * 
+         * Given the array nums after the rotation, return the minimum element of this array.
+         * 
+         * You must write an algorithm that runs in O(log n) time.
+         * 
+         * Example 1:
+         * Input: nums = [3,4,5,1,2]
+         * Output: 1
+         * 
+         * Example 2:
+         * Input: nums = [4,5,6,7,0,1,2]
+         * Output: 0
+         */
+
+        int[] array3 = {4, 5, 6, 7, 9, 11, 0, 1, 2};
+        int result3 = binarySearchMinimum(array3);
+        if (result3 == -1) {
+            System.out.println("The number is not in the array");
+        } else {
+            System.out.println("The lowest number is " + result3);
+        }
+
+
 
     }
 
@@ -65,24 +95,24 @@ public class study {
     }
 
     // Binary Search with rotated array
-    public static int binarySearchRotatedArray(int[] array, int target) {
-        int left = 0;
-        int right = array.length - 1;
+    public static int binarySearchRotatedArray(int[] array, int target) {           // Array = [6, 7, 8, 9, 1, 2, 3, 4, 5] target = 4
+        int left = 0;                                                               // left = 0, array[0] = 6
+        int right = array.length - 1;                                               // right = 8, array[8] = 5
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (array[mid] == target) {
-                return mid;
+        while (left <= right) {                                                     // 0 <= 8 , remember it's checking the array length not the index
+            int mid = left + (right - left) / 2;                                    // mid = 0 + (8 - 0) / 2 = 4, array[4] = 1
+            if (array[mid] == target) {                                             // if 1 == 4, false
+                return mid;                                                         // return 4, false
             }
-            if (array[left] <= array[mid]) { // If the left side is sorted
-                if (target >= array[left] && target < array[mid]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
+            if (array[left] <= array[mid]) { // If the left side is sorted          // if 6 <= 1, false
+                if (target >= array[left] && target < array[mid]) {                 
+                    right = mid - 1;                                                
+                } else {                                                                                    
+                    left = mid + 1;                                                 
                 }
-            } else { // If the right side is sorted
-                if (target > array[mid] && target <= array[right]) {
-                    left = mid + 1;
+            } else { // If the right side is sorted                                 // if 6 > 1, true
+                if (target > array[mid] && target <= array[right]) {                // if 4 > 1 && 4 <= 5, true
+                    left = mid + 1;                                                 // left = 4 + 1 = 5  = [2, 3, 4, 5]
                 } else {
                     right = mid - 1;
                 }
@@ -90,5 +120,23 @@ public class study {
         }
         return -1; // Add the missing return statement
     }
+
+    // Binary Search finding the minimum in both rotated and non-rotated array
+    public static int binarySearchMinimum(int[] array) {                            // Array = [4, 5, 6, 7, 9, 11, 0, 1, 2]
+        int left = 0;                                                               // left = 0                        
+        int right = array.length - 1;                                               // right = 8
+        // Check if array is empty
+        while (left < right) {
+            int mid = left + (right - left) / 2;                                    // mid = 0 + (8 - 0) / 2 = 4, array[4] = 9      // mid = 5 + (8 - 5) / 2 = 6, array[6] = 0
+            if (array[mid] < array[right]) {                                        // if 9 < 2, false                              // if 0 < 2, true
+                right = mid;
+            } else {                                                                // if 9 > 2, true
+                left = mid + 1;                                                     // left = 4 + 1 = 5
+            }
+        }
+        return array[left];
+    }
+
+    // Binary Search with 2D array
 }
 
