b509afa7b67fd68dfebda2fc6b319f9d3e6b63e9
Quoc Bao Dinh Le <qle17@student.gsu.edu>
2024-03-06 15:24:48 -0500

    notes

--- Week 9 - Stacks & Queues/week9.py
+++ Week 9 - Stacks & Queues/week9.py
@@ -128,3 +128,74 @@ def stack():
     print(stack)
     
 print(stack())
+
+"""
+3/6/2024 - Wednesday
+
+Call stack is a stack data structure that stores information about the active subroutines of a computer program.
+Recursion is not memory free, it uses the call stack to store the return address of the function.
+
+F(4) = 4 * F(3) = 4 * 3 * F(2) = 4 * 3 * 2 * F(1) = 4 * 3 * 2 * 1 * F(0) = 4 * 3 * 2 * 1 * 0 = 24
+
+| F(0) |    F(0) = 0
+| F(1) |    F(1) = 1
+| F(2) |    F(2) = 2  Recursion Fall Stack
+| F(3) |    F(3) = 6
+| F(4) |    F(4) = 24
+|______|
+ Stack  -> O(n)
+ 
+ There is no Recursion without a Call Stack, never.
+ Recursive Binary has extra memory due to the call stack.
+ 
+ 
+ New Topic: Queues
+ 
+    Queue is a data structure that follows the First In First Out (FIFO) principle.
+    Array and Linked List are the two main ways to implement a queue.
+    
+    Head: 5 -> 4 -> 3 -> 2 -> 1 -> Null
+    
+    Base Average: 5 | 4 | 9 | 7 | 11 | 8
+    
+    Pro:
+    - Fast operations
+    - Fast access to the first element
+    - Easy to implement
+    
+    Con:
+    - Fixed size
+    - Slow access to other elements
+    - Slow search
+    - Slow to remove elements
+    - Slow to insert elements
+    
+    Time complexity:
+    - Access: O(n) since we're handling the element one by one
+    Space complexity:
+    - O(n) -> Pre-Allocation
+    
+    When in queue a used space is called a base. To prevent overflow, we can use a circular queue.
+    That way,  can use the empty spaces in the base.
+    
+    Double Ended Queue (Deque) is a queue that allows insertion and removal of elements from both ends.
+ 
+    Head-> 1, 4, 2, 3, 7 <-Tail
+    Find the maximum element in the queue.
+    In constant time, we can find the maximum element in the queue.
+    The maximum element is always at the head or tail of the queue.
+    You need an extra data structure to keep track of the maximum element.
+    
+                Head
+    Main Queue: 1,  4,                      2,      3
+    Max Queue:  1 -> 4 (replace 1 with 4) -> 4, 2 -> 4, 3
+    If a new item, i, is greater than the last element in the max queue, pop the last element and append i.
+        remove from tail of Max Q
+    equal to Max Q
+    
+    Function:
+    # a new item i
+      while i > tail item of Max Q
+        remove from tail of Max Q
+      enqueue to Max Q
+"""
