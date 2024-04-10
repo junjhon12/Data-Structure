5f5927d4607525565e063621b062501a0c5d438a
Quoc Bao Dinh Le <qle17@student.gsu.edu>
2024-04-10 13:45:09 -0400

    finish the week 13

--- Week 13 - Tree and Priority Queue/week13.py
+++ Week 13 - Tree and Priority Queue/week13.py
@@ -82,7 +82,7 @@ Logic(Recursive):
 We use a SET to avoid infinite loops.
 We use a stack to keep track of the nodes.
         
-edges = [[0,1], [0,2], [1,3], [1,4]]
+edges = [[0,1], [0,2], [0,3], [1,4]]
 
 0 - [1 - 2 - 3]
 1 - [0 - 4]
@@ -101,6 +101,8 @@ for node1, node2 in edges:
     adj_list[node2].append(node1)
     
 visited = set()
+
+dfs stands for Depth First Search
 
 def dfs(node, visited):     Cycle Checking
     if node in visited:
@@ -133,16 +135,96 @@ def connected_components(n, edges):
             components += 1
     return components
     
+Graph Valid Tree:
+
+    
 """
 
-def dfs(node, prev, visited, adj_list):
-    if node in visited:
-        return False
-    visited.add(node)
-    for neighbor in adj_list[node]:
-        if neighbor == prev:
-            continue
-        if not dfs(neighbor, node, visited, adj_list):
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def dfs(node, prev, visited, adj_list):
+        if node in visited:
             return False
-        return True
+        visited.add(node)
+        for neighbor in adj_list[node]:
+            if neighbor == prev:
+                continue
+            if not dfs(neighbor, node, visited, adj_list):
+                return False
+            return True
+        
+
     
+"""
+4/10/2024
+
+A Node property of a min heap is that the parent is [ALWAYS] less than the children.
+Subtree is recursively a heap in each node.
+
+Example:            10
+                11     15
+            20  32   45  50
+            
+Left Child: 2i + 1
+Right Child: 2i + 2
+Parent: (i - 1) // 2
+
+What's the difference between priority queue and heap queue thread safety?
+Priority Queue is thread safe.
+Heap Queue is not thread safe.
+
+Thread unsafe means that it is not safe to use in a multi-threaded environment.
+
+Out of heap queue and priority queue, which one is based off the other?
+Heap Queue is based off Priority Queue.
+"""
+
+"""
+                        3
+                    5       NULL
+                8       10
+            9    NULL  NULL  NULL
+            
+A Balance Tree is a tree where the height of the left and right subtrees of any node differ by at most 1.
+"""
+# Traversal should be converted to 
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def check_balance(self, root):
+        def depth_first_search(root):
+            if root is None:
+                return [True, 0]
+            left_subtree = depth_first_search(root.left)
+            right_subtree = depth_first_search(root.right)
+            #Balance is the difference between the left and right subtree heights
+            balance = (left_subtree[0] and right_subtree[0]) and (abs(left_subtree[1] - right_subtree[1]) <= 1)
+            
+            return [balance, max(left_subtree[1], right_subtree[1]) + 1]
+        return depth_first_search(root)[0]
+
+    
+    
+root = Node(3)
+root.left = Node(5)
+root.right = Node(None)
+root.left.left = Node(8)
+root.left.right = Node(10)
+root.left.left.left = Node(9)
+print(root.check_balance(root))
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+root.right.left = Node(6)
+root.right.right = Node(7)
+print(root.check_balance(root))
